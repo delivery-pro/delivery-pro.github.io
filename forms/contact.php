69e354a3ddefa08f696581709fa937becde4059d
so-spe <93432319+so-spe@users.noreply.github.com>
2023-08-23 15:17:19 +0200

    TODO list
    uploaded some assets, custom PHP Email Form

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // https://github.com/ChrishonWyllie/PHP-Email-Form
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  // $human = intval($_POST['human']);
+  $from = $email; 
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // The address that the email will be sent to
+  $to = 'deliverypro.cloud@gmail.com'; 
+  
+  $subject = $_POST['subject'];
+  
+  $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // Check if name has been entered
+  if (!$_POST['name']) {
+    $err = 'Please enter your name!';
+  }
+  
+  // Check if email has been entered and is valid
+  if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $err = 'Please enter a valid email address!';
+  }
+  
+  //Check if message has been entered
+  if (!$_POST['message']) {
+    $err = 'Please enter your message!';
+  }
+  //Check if simple anti-bot test is correct
+  // if ($human !== 5) {
+  //   $err = 'Your anti-spam is incorrect';
+  // }
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  // If there are no errors, send the email
+  if (!$err) {
+    if (mail ($to, $subject, $body, $from)) {
+      echo 'Your message has been sent. Thank you!';
+    } else {
+      die("Sorry there was an error sending your message. $err");
+    }
+  }
 ?>
